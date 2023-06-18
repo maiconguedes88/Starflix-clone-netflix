327552407596e8dc917673b834746d32e46ae5e4
maiconguedes88 <maicon_jau@hotmail.com>
2023-06-18 11:29:49 -0300

    atualização carroçel II

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -4,13 +4,13 @@ $('.owl-carousel').owlCarousel({
     nav:false,
     responsive:{
         0:{
-            items:1
+            items:4
         },
         600:{
-            items:3
+            items:6
         },
         1000:{
-            items:5
+            items:8
         }
     }
 })
